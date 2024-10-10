ff027a12b5b67a8b0b0ae33af87df2c9992ee66b
Gregory Gerard <wako057@gmail.com>
2024-10-10 13:39:35 +0200

    feat: value object creation

--- src/message.ts
+++ src/message.ts
@@ -1,7 +1,26 @@
 
+export class MessageTooLongError extends Error {};
+export class EmptyMessageError extends Error {};
+
 export type Message = {
     id: string,
-    text: string,
+    text: MessageText,
     author: string,
     publishedAt: Date
 };
+
+export class MessageText {
+    private constructor(readonly value: string){}
+
+    static of (text: string) {
+        if (text.length > 280) {
+            throw new MessageTooLongError()
+        }
+
+        if (text.trim().length === 0) {
+            throw new EmptyMessageError();
+        }
+
+        return new MessageText(text); 
+    }
+}

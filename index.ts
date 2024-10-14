569822f8180dc9389620de4b60216e58b265da23
Gregory Gerard <wako057@gmail.com>
2024-10-14 17:48:47 +0200

    feat: implement wall & follow cli command

--- index.ts
+++ index.ts
@@ -7,6 +7,9 @@ import { FileSystemMessageRepository } from './src/infra/message.fs.repository';
 import { PostMessageCommand, PostMessageUseCase } from './src/application/usecase/post-message.usecase';
 import { ViewTimelineUseCase } from './src/application/usecase/view-timeline.usecase';
 import { EditMessageCommand, EditMessageUseCase } from './src/application/usecase/edit-message.usecase';
+import { FollowUserCommand, FollowUserUseCase } from './src/application/usecase/follow-user.usecase';
+import { FileSystemFolloweeRepository } from './src/infra/followee.fs.repository';
+import { ViewWallUseCase } from './src/application/usecase/view-wall.usecase';
 
 
 
@@ -15,6 +18,9 @@ const messageRepository = new FileSystemMessageRepository();
 const postMessageUseCase = new PostMessageUseCase(messageRepository, dateProvider);
 const viewTimelineUseCase = new ViewTimelineUseCase(messageRepository, dateProvider);
 const editMessageUseCase = new EditMessageUseCase(messageRepository);
+const followeeRepository = new FileSystemFolloweeRepository();
+const followUserUserCase = new FollowUserUseCase(followeeRepository); 
+const viewWallUseCase = new ViewWallUseCase(messageRepository, followeeRepository, dateProvider);
 const program = new Command();
 
 program
@@ -64,11 +70,46 @@ program
             })
     )
     .addCommand(
+        new Command("follow")
+            .argument("<user>", "The current user")
+            .argument("<user-to-follow>", "The user to follow")
+            .action(async (user, userToFollow) => {
+                const followUserCommand: FollowUserCommand = {
+                    user,
+                    userToFollow
+                };
+
+                try {
+                    await followUserUserCase.handle(followUserCommand);
+                    console.log(`✅ Tu suis maintenant ${userToFollow}`);
+                    console.table(await messageRepository.getMessages());
+                    process.exit(0);
+                } catch (err) {
+                    console.error("❌", err);
+                    process.exit(1);
+                }
+            })
+    )
+    .addCommand(
         new Command("view")
             .argument("<user>", "The user to view the timeline of")
             .action(async (user) => {
                 try {
                     const timeline = await viewTimelineUseCase.handle({ user });
+                    console.table(timeline);
+                    process.exit(0);
+                } catch (err) {
+                    console.error("❌", err);
+                    process.exit(1);
+                }
+            })
+    )
+    .addCommand(
+        new Command("wall")
+            .argument("<user>", "The user to view the wall of")
+            .action(async (user) => {
+                try {
+                    const timeline = await viewWallUseCase.handle(user);
                     console.table(timeline);
                     process.exit(0);
                 } catch (err) {
